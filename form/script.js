90250bcd6cfdd746d6a16e950c83da6b01904fa2
Tatiana Ermakova <46784317+termakova@users.noreply.github.com>
2020-03-26 20:52:08 +0300

    added render

--- form/script.js
+++ form/script.js
@@ -20,25 +20,46 @@ class Employee extends Person {
     }
 }
 
+const render = function (elements, root) {
+    root.innerHTML = '';
+
+    elements.forEach(element => {
+
+    let newSomeone = document.createElement("li");
+    let newEmployeeName = document.createElement("option");
+
+    console.log("!!!! " + element.personName)
+
+    newEmployeeName.value = element.personName;
+    newEmployeeName.innerHTML = element.personName;
+    newSomeone.innerHTML = `${element.personName} (${element.personPhone}) ${element.enteredJobTitle ? element.enteredJobTitle : ''}`;
+
+    root.appendChild(newSomeone);
+    document.getElementById("employee_names").appendChild(newEmployeeName);
+
+    });
+}
+
+
 const onPersonSubmit = function () {
 
     let enteredName = document.getElementById("name");
     let enteredPhone = document.getElementById("phone");
     let personsList = document.getElementById("persons_list");
-    let employeeDropdown = document.getElementById("employee_names");
+ //   let employeeDropdown = document.getElementById("employee_names");
     
-    let newPerson = document.createElement("li");
-    let newEmployeeName = document.createElement("option");
+ //   let newPerson = document.createElement("li");
+ //   let newEmployeeName = document.createElement("option");
 
-    // newPerson.innerHTML = enteredName.value + ": " + enteredPhone.value;
-    newPerson.innerHTML = `${enteredName.value}:${enteredPhone.value}`;
-    newEmployeeName.value = enteredName.value;
-    newEmployeeName.innerHTML = enteredName.value;
+ //   newPerson.innerHTML = `${enteredName.value}: ${enteredPhone.value}`;
+ //   newEmployeeName.value = enteredName.value;
+ //   newEmployeeName.innerHTML = enteredName.value;
 
-    personsList.appendChild(newPerson);
-    employeeDropdown.appendChild(newEmployeeName);
+ //   personsList.appendChild(newPerson);
+ //   employeeDropdown.appendChild(newEmployeeName);
 
     persons.push(new Person(enteredName.value, enteredPhone.value));
+    render(persons, personsList);
         
     console.log(persons);
 
@@ -49,17 +70,18 @@ const onEmployeeSubmit = function () {
     let enteredJobTitle = document.getElementById("position");
     let chosenEmployeeName = document.getElementById("employee_names").value;
     let employeesList = document.getElementById("employees_list");
-    const pnone =  persons.find((person) => person.personName === chosenEmployeeName).personPhone;
+    const phone =  persons.find((person) => person.personName === chosenEmployeeName).personPhone;
 
-    let newEmployee = document.createElement("li");
+//    let newEmployee = document.createElement("li");
 
-    const employee = new Employee(chosenEmployeeName, pnone, enteredJobTitle.value);
+//    const employee = new Employee(chosenEmployeeName, pnone, enteredJobTitle.value);
 
-    newEmployee.innerHTML = employee.personName + " (" + pnone + "): " + employee.employeeJobTitle;
+//    newEmployee.innerHTML = `${employee.personName}(${pnone}): ${employee.employeeJobTitle}`;
 
-    employeesList.appendChild(newEmployee);
+//    employeesList.appendChild(newEmployee);
 
-    employees.push(new Employee(chosenEmployeeName, pnone, enteredJobTitle.value));
+    employees.push(new Employee(chosenEmployeeName, phone, enteredJobTitle.value));
+    render(employees, employeesList)
     console.log(employees);
 
 }

7fb010eecfc1a786c76f36dadba39523340f0bea
AndreyKuryukin <nuveum@gmail.com>
2020-03-27 13:08:01 +0300

    HW

--- script.js
+++ script.js
@@ -41,4 +41,52 @@ for (let i = 0; i < 10; ++i) {
 
 [1,2,3].forEach(() => {});
 
-document.getElementById('child').className = "child_2"
+document.getElementById('child').className = "child_2";
+
+const tree = {
+    a: 1,
+    children: [
+        {
+            a: 2,
+            children: [
+                {
+                    a: 4,
+                    children: []
+                },
+                {
+                    a: 5,
+                    children: []
+                }
+            ]
+        },
+        {
+            a: 3,
+            children: [
+                {
+                    a: 6,
+                    children: []
+                },
+                {
+                    a: 7,
+                    children: [
+                        {
+                            a: 8,
+                            children: [
+                                {
+                                    a: 9,
+                                    children: []
+                                },
+                                {
+                                    a: 10,
+                                    children: []
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+// Вывести в консоль цепочки родителей для каждого самого глубокого чилдрена

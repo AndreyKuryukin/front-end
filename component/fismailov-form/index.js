afd7da3704e78ac05d60852770be6b42ee6d9cbd
Farid Ismailov <fismailov@wiley.com>
2020-03-28 11:41:25 +0300

    fix fismailov-form duplicates issue

--- component/fismailov-form/index.js
+++ component/fismailov-form/index.js
@@ -1,61 +1,59 @@
 class Person {
-  name;
-  tel;
+	name;
+	tel;
 
-  constructor(name, tel) {
-    this.name = name;
-    this.tel = tel;
-  }
+	constructor(name, tel) {
+		this.name = name;
+		this.tel = tel;
+	}
 }
 
 class Employee extends Person {
-  jobTitle;
+	jobTitle;
 
-  constructor(name, jobTitle, tel) {
-    super(name, tel);
-    this.jobTitle = jobTitle;
-  }
+	constructor(name, jobTitle, tel) {
+		super(name, tel);
+		this.jobTitle = jobTitle;
+	}
 }
 
 let persons = [];
 let employees = [];
 
+function cleanUp() {
+	document.getElementById('name').value = '';
+	document.getElementById('tel').value = '';
+	document.getElementById('employeeName').innerHTML = '';
+}
+
 function render(elements, root) {
-  root.innerHTML = '';
-  elements.forEach(element => {
-    let personDisplay = document.createElement("li");
-    let employeeOption = document.createElement("option");
+	cleanUp();
+	elements.forEach(element => {
+		let personDisplay = document.createElement("li");
+		let employeeOption = document.createElement("option");
 
-    employeeOption.value = `${element.name}`;
-    employeeOption.innerHTML = `${element.name}`;
-    personDisplay.innerHTML = `${element.name}: ${element.tel} - ${element.jobTitle ? element.jobTitle : ''}`;
+		employeeOption.value = `${element.name}`;
+		employeeOption.innerHTML = `${element.name}`;
+		personDisplay.innerHTML = `${element.name}: ${element.tel} - ${element.jobTitle ? element.jobTitle : ''}`;
 
-    root.appendChild(personDisplay);
-    document.getElementById('employeeName').appendChild(employeeOption);
-  });
+		root.appendChild(personDisplay);
+		document.getElementById('employeeName').appendChild(employeeOption);
+	});
 }
 
 function createPerson() {
-  let name = document.getElementById('name');
-  let tel = document.getElementById('tel');
+	let name = document.getElementById('name').value;
+	let tel = document.getElementById('tel').value;
 
-  persons.push(new Person(name.value, tel.value));
-  render(persons, document.getElementById('personsList'));
-
-  name.value = "";
-  tel.value = "";
+	persons.push(new Person(name, tel));
+	render(persons, document.getElementById('personsList'));
 }
 
 function createEmployee() {
-  let employeeName = document.getElementById('employeeName').value;
-  let jobTitle = document.getElementById('jobtitle').value;
+	let employeeName = document.getElementById('employeeName').value;
+	let jobTitle = document.getElementById('jobtitle').value;
+	const person = persons.find((prsn) => prsn.name === employeeName);
 
-  const person = persons.find((prsn) => prsn.name === employeeName);
-
-  employees.push(new Employee(person.name, jobTitle,person.tel)); //where should I get tel?
-  render(employees, document.getElementById('employeeList'));
-  //receiving undefined: undefined here...
-
-  // employeeName.value = "";
-  // jobTitle.value = "";
+	employees.push(new Employee(person.name, jobTitle, person.tel));
+	render(employees, document.getElementById('employeeList'));
 }
